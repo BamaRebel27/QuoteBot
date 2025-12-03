853da65e1a741c8922c9be76e6706ed0306595da
BamaRebel27 <43051249+BamaRebel27@users.noreply.github.com>
2025-12-03 14:22:17 -0600

    Update aquote.js

--- EchoBotJS/commands/aquote.js
+++ EchoBotJS/commands/aquote.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
 const fs = require('fs');
 
+// Where to store your quotes.
+const quotesFilePath = 'quotes.json';
+
 // Command to get a random quote: /aquote
 module.exports = {
     cooldown: 600,
@@ -13,41 +16,37 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         // Load jsonData
-        const data = JSON.parse(fs.readFileSync('quotes.json'));
+        const quotesJsonData = JSON.parse(fs.readFileSync(quotesFilePath));
 
         // Get number string and check it. Then convert to a variable
-        const numberInput = interaction.options.getNumber('number');
-        var quoteID;
+        const quoteIdInput = interaction.options.getNumber('number');
+        var quoteIndex;
+        const quoteTotal = quotesJsonData.length
 
-        if (numberInput <= quoteTotal && numberInput >= 1) {
-            quoteID = Number(numberInput);
+        if (quoteIdInput <= quoteTotal && quoteIdInput >= 1) {
+            quoteIndex = Number(quoteIdInput);
             await interaction.deferReply()
         } else {
             await interaction.reply({
                 content: `The input must be a number 1-${quoteTotal}!`,
                 flags: MessageFlags.Ephemeral
-                
             })
             return;
         }
+
         
-        var i = quoteID--
+        var quoteID = quoteIndex--
 
         // const data for the embed
-        const author = (data[quoteID].author);
-        const quote = (data[quoteID].quote);
+        const author = (quotesJsonData[quoteIndex].author);
+        const quote = (quotesJsonData[quoteIndex].quote);
 
         // const for embed data
         const quote_addedEmbed = new EmbedBuilder()
         .setColor(0xb07a70)
-        .setTitle(`Stored Quote - #${i}`)
+        .setTitle(`Stored Quote - #${quoteID}`)
         .setDescription(`"*${quote}*" - ${author}`)
 
         interaction.editReply({ embeds: [quote_addedEmbed] });
     }
 };
-
-
-
-
-
