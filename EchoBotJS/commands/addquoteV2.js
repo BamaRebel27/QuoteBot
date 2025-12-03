8255cf9e654bee7e407815fa748c2eda43a055e6
BamaRebel27 <43051249+BamaRebel27@users.noreply.github.com>
2025-12-03 14:21:07 -0600

    Update addquoteV2.js

--- EchoBotJS/commands/addquoteV2.js
+++ EchoBotJS/commands/addquoteV2.js
@@ -1,6 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
 const fs = require('fs');
-const { json } = require('stream/consumers');
+
+// Where to store your quotes... Make a file name.json, name cane be whatever you choose, the file contents should be an empty [].
+const quotesFilePath = 'quotes.json';
 
 // Command to add a new quote: /addquote
 module.exports = {
@@ -18,20 +20,20 @@ module.exports = {
                 .setRequired(true)),
      async execute(interaction) {
 
-         // Const for role and checking if member has role to execute command
+        // Const for role and checking if member has role to execute command
         const requiredRoleId = '1445508130430648331'
         
         if (!interaction.member.roles.cache.has(requiredRoleId)) {
             return interaction.reply({ content: 'You do not have the required role!', flags: MessageFlags.Ephemeral })
         }
-         
+
         try {
             // Strings for JS object
             const quote = interaction.options.getString('quote');
             const author = interaction.options.getString('author')
 
             // Loading quotes.json
-            const data = JSON.parse(fs.readFileSync('quotes.json'));
+            const quotesJsonData = JSON.parse(fs.readFileSync(quotesFilePath));
 
             // Array of objects for jsonString
             const newQuote = [
@@ -39,8 +41,8 @@ module.exports = {
             ];
 
             // Get new quoteID
-            let i = data.length
-            let quoteID = ++i
+            let quoteTotal = quotesJsonData.length
+            let quoteID = ++quoteTotal
 
             // const for embed data
             const quote_addedEmbed = new EmbedBuilder()
@@ -51,23 +53,23 @@ module.exports = {
             // Function to add data to the JSON file
             async function addToArrayInJsonFile(newData) {
                 try {
-                    let existingData = [];
+                    let existingQuotes = [];
             
                     // 1. Read the existing JSON file
-                    if (fs.existsSync('quotes.json')) {
-                        const fileContent = await fs.promises.readFile('quotes.json', 'utf8');
-                        // 2. Parse the content into a JavaScript array
-                        existingData = JSON.parse(fileContent);
+                    if (fs.existsSync(quotesFilePath)) {
+                        existingQuotes = quotesJsonData
+                    } else {
+                        return interaction.reply({ content: 'The storage file is missing!', flags: MessageFlags.Ephemeral})
                     }
             
                     // 3. Append the new data
-                    existingData.push(...newData); // Using spread syntax to add multiple items
+                    existingQuotes.push(...newData); // Using spread syntax to add multiple items
             
                     // 4. Convert the updated array back to a JSON string
-                    const updatedJsonString = JSON.stringify(existingData, null, 2); // null, 2 for pretty printing
+                    const updatedQuotes = JSON.stringify(existingQuotes, null, 2); // null, 2 for pretty printing
             
                     // 5. Write the updated JSON string back to the file
-                    await fs.promises.writeFile('quotes.json', updatedJsonString, 'utf8');
+                    await fs.promises.writeFile(quotesFilePath, updatedQuotes, 'utf8');
                     await interaction.reply({ embeds: [quote_addedEmbed] });
             
                 } catch (error) {
@@ -83,4 +85,3 @@ module.exports = {
      }
     
 }};
-
